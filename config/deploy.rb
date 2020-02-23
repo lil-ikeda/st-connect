6487c512092d756a478f6adc30a1a50a385fa290
Naohito Ikeda <ikepo.japon@gmail.com>
2020-02-23 15:39:26 +0900

    fix git acount name

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'st-connect'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:lilikeda/st-connect.git'
+set :repo_url,  'git@github.com:lil-ikeda/st-connect.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
