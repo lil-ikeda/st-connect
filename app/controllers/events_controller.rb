e3b4f5cb5b2e916f25dfcf754197edea513ab13c
Naohito Ikeda <ikepo.japon@gmail.com>
2020-02-26 17:55:46 +0900

    イベント参加・不参加機能の実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :join, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :join, :unjoin, :destroy]
   
   def index
     @events = Event.all.order(date: :asc).page(params[:page]).per(10)
@@ -19,27 +19,40 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event_user = EventUser.where(event_id: @event.id, user_id: current_user.id)[0]
   end
-
+  
   def edit
   end
-
+  
   def update
     @event.update(event_params)
     redirect_to event_path
   end
-
+  
   def destroy
     @event.destroy
     redirect_to root_path
   end
-
-  private
-  def event_params
-    params.require(:event).permit(:name, :place, :date, :time, :image)
+  
+  def join
+    EventUser.create(user_id: current_user.id, event_id: @event.id)
+    redirect_to event_path
+  end
+  
+  def unjoin
+    event_user = EventUser.where(user_id: current_user.id, event_id: params[:id])[0]
+    event_user.destroy
+    redirect_to event_path(params[:id])
   end
 
+  private
   def set_event
     @event = Event.find(params[:id])
   end
+
+  def event_params
+    params.require(:event).permit(:name, :place, :date, :time, :image).merge(owner: current_user.id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :evnet_users, dependent: :destroy
+  has_many :events, through: :event_users
+  
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
   end
   
+  mount_uploader :image, ImageUploader
 end
