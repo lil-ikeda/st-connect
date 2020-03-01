0c39a4e3ea5c75d85c66412a6acab3ef93ee63a3
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-01 15:16:28 +0900

    ユーザー詳細ページのcssとバグの修正
    # WHAT
    ・全てのユーザー詳細ページに対して、current_userの情報を表示させてしまっていたため、改修
    ・選択中のタブを目立つようにcssを修正
    
    # WHY
    ・バグ動作の改修のため
    ・UXをあげるため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @room = (current_user.rooms & @user.rooms)[0]
     @events = Event.where(owner: @user.id)
-    @following_users = current_user.followings
-    passive_relationships = Relationship.where(following_id: current_user.id)
+    @following_users = @user.followings
+    passive_relationships = Relationship.where(following_id: @user.id)
     @followed_users = User.where(id: passive_relationships.select(:user_id))
-    @matchers = current_user.matchers
+    @matchers = @user.matchers
   end
 
 end
