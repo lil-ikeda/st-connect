0aac10e6f35fb150ea3e4d0fab0b68b310a24b56
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-02 19:46:06 +0900

    リクエストの通知機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    passwords: 'users/passwords',
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
