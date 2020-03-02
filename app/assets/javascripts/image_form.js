72ab1d8a15020d8fae8b0676b4305e475ef419d2
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-02 12:39:29 +0900

    ユーザー新規登録ページ/写真投稿フォームのプレビュー機能実装

--- app/assets/javascripts/image_form.js
+++ app/assets/javascripts/image_form.js
@@ -21,3 +21,30 @@ $(function(){
     reader.readAsDataURL(file);
   });
 });
+
+
+// ユーザー新規登録
+
+$(function(){
+  $fileField = $('#regitration_file')
+ 
+  // 選択された画像を取得し表示
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#regitration_img_field");
+ 
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          width: "100%",
+          class: "regitration_preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
