c6359892732932363124e0904a731c125abda5c9
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-05 16:27:57 +0900

    デプロイテスト

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,7 +17,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-      "user_test_login.jpeg"
+      # "user_test_login.jpeg"
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
