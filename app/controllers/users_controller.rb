a65bf4ebec3c5d6a0c6f5bc4d6754212efc879a7
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-07 15:25:57 +0900

    ユーザー詳細ページのバグ改修
    # WHAT
    ・ユーザー詳細ページの```current_user```を使っている箇所に```if user_signed_in?```の条件分岐を記述
    
    # WHY
    ・ユーザーが未ログイン時にユーザー詳細ページに遷移するとエラーが発生してしまうため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @room = (current_user.rooms & @user.rooms)[0]
     @events = Event.where(owner: @user.id)
     @following_users = @user.followings
     passive_relationships = Relationship.where(following_id: @user.id)
     @followed_users = User.where(id: passive_relationships.select(:user_id))
     @matchers = @user.matchers
+    if user_signed_in?
+      @room = (current_user.rooms & @user.rooms)[0]
+    end
   end
 
 end
