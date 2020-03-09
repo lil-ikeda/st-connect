98528f98390b2a6ca9f8d9f4f59c7c9cf4514996
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-09 13:10:43 +0900

    コメント機能修正
    # WHAT
    ・ゲストユーザーである場合とそうでない場合で、コメントの表示の仕方を条件分岐
    
    # WHY
    ・ゲストユーザーがコメントを投稿した際に、JSで出力したimageが表示されない状態になっていたため

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,7 +1,30 @@
 $(function(){
   function buildHTML(comment){
     // コメントがある場合のみコメントを追加
-    if (comment.text) {
+    if (comment.text && comment.email == "guest@example.com") {
+      var html = `<div class="comment_form__content">
+                    <div class="comment_left">
+                      <a class="comment_links" href="/users/${comment.user_id}">
+                        <img src="/assets/user_test_login-3415ac9caf57877a44a7b2dced4bcc8e8dfd96c7fbfdc3647e8c2cff8f9ab214.jpg" class="comment_form__content--img">
+                      </a>
+                    </div>
+                    <div class="comment_right">
+                      <strong>
+                        <a class="comment_form__content--name" href="/users/${comment.user_id}">${comment.user_name}</a>
+                      </strong>
+                      <p class="comment_form__content--text">
+                        ${comment.text}
+                      </p>
+                    </div>
+                    <div class="comment_delete">
+                      <div class="comment_time">
+                        今
+                      </div>
+                      <a class="comment_delete__link" rel="nofollow" data-method="delete" href="/events/${comment.event_id}/comments/${comment.id}">削除</a>
+                    </div>
+                  </div>`
+      return html;
+    } else if (comment.text) {
       var html = `<div class="comment_form__content">
                     <div class="comment_left">
                       <a class="comment_links" href="/users/${comment.user_id}">
--- app/views/comments/create.json.jbuilder
+++ app/views/comments/create.json.jbuilder
@@ -4,3 +4,4 @@ json.user_id  @comment.user.id
 json.user_name  @comment.user.name
 json.user_image @comment.user.image.url
 json.event_id @comment.event.id
+json.email @comment.user.email
