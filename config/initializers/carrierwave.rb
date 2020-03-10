0005a0863bb1481167e8dc52f03d3a74b3c387f1
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-10 17:19:49 +0900

    circleci検証
    # WHAT
    ・エラー箇所をコメントアウト

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_provider = 'fog/aws'
+  # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
