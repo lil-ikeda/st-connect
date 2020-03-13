7c05eaaa56e9cd663022b47d230d9b05873475ad
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-13 18:35:26 +0900

    events_controllerのテストコメントアウト
    # WHAT
    ・testコードをコメントアウト

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe EventsController, type: :controller do
-  describe 'GET #new' do
-    it "renders the :new template" do
-      get :new
-      expect(response).to render_template :new
-    end
-  end
+  # describe 'GET #new' do
+  #   it "renders the :new template" do
+  #     get :new
+  #     expect(response).to render_template :new
+  #   end
+  # end
 
 
 #   # describe 'GET #edit' do
