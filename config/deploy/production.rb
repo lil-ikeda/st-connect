d194666d68de9ee9a42c4928c2dd90f55b6da076
Naohito Ikeda <ikepo.japon@gmail.com>
2020-03-13 15:39:15 +0900

    トップページのバグ修正
    # WHAT
    ・jsに対して、turbolinksの読み込みの記述を削除

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '54.178.158.206', user: 'ec2-user', roles: %w[app db web]
-server '54.249.15.67', user: 'ec2-user', roles: %w{app db web}
+# server '54.249.15.67', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
